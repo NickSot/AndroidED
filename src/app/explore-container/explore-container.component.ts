dcf494c61af7bbb9d461c42fb57f75a7ed2f77e8
NickSot <nikola.s.sotirov@gmail.com>
2021-10-19 20:33:06 +0200

    pending tests on the bluetooth connection

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 
 @Component({
   selector: 'app-explore-container',
@@ -10,11 +11,16 @@ export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
   height: number = 180.0;
 
-  constructor() { }
+  constructor(private bluetoothSerial: BluetoothSerial) {
+    bluetoothSerial = new BluetoothSerial();
+  }
 
   //Bluetooth module address: 00:0C:BF:19:A2:59
   ngOnInit() {
     //initialize bluetooth connection...
+    this.bluetoothSerial.connect("00:0C:BF:19:A2:59").toPromise().then((device) => {
+      console.log('Success!!!');
+    });
   }
 
   ngOnClick() {
