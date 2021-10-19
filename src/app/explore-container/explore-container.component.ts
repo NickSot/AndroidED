b97520a5df6aeda8fff9999625fc867cb18d2292
NickSot <nikola.s.sotirov@gmail.com>
2021-10-19 19:57:46 +0200

    reasearch for libraries that have bluetooth serial communication

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -5,16 +5,19 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './explore-container.component.html',
   styleUrls: ['./explore-container.component.scss'],
 })
+
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
   height: number = 180.0;
 
   constructor() { }
 
-  ngOnInit() {}
+  //Bluetooth module address: 00:0C:BF:19:A2:59
+  ngOnInit() {
+    //initialize bluetooth connection...
+  }
 
   ngOnClick() {
     console.log(`The calculated distance is: ${this.height/2}`);
-
   }
 }
